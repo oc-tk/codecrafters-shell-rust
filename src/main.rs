b4fd5b8c990fcacdc61c863c36b6ba45254e781a
Tomasz <tspamm3r@gmail.com>
2024-05-24 21:33:42 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,14 @@ fn main() {
         let command = input.trim();
 
         match command {
-            x  if x.to_string().contains("exit") => process::exit(command.chars().last()),
+            x  if x.to_string().contains("exit") => {
+                if let Some(last_char) = command.chars().last() {
+                    match last_char.to_string().parse::<i32>() {
+                        Ok(status) => process::exit(status),
+                        Err(e) => println!("Failed to parse the status code: {}", e),
+                    }
+                }
+            },
             _ => println!("{command}: command not found"),
         }
     }
