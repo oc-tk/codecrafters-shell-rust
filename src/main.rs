8160031ad1a6fdb04d42d1de41aac8e0e4aa3c4e
Tomasz <tspamm3r@gmail.com>
2024-05-24 21:04:06 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ fn main() {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
 
-    input.strip_suffix("\r\n")
+    let inputFormatted = input.strip_suffix("\r\n")
          .or(input.strip_suffix("\n"))
          .unwrap();
 
-    print!("{input}: command not found");
+    print!("{inputFormatted}: command not found");
 }
