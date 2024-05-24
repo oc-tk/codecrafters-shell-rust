1646fa5a60a75c3d169af7254253ef53353279a2
Tomasz <tspamm3r@gmail.com>
2024-05-24 21:17:05 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@ use std::io::{self, Write};
 
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    // Wait for user input
-    //let stdin = io::stdin();
-    // let mut input = String::new();
-    //stdin.read_line(&mut input).unwrap();
-
-    let lines = io::stdin().lines();
-    for line in lines {
-        let command = line.unwrap();
+        let command = input.trim();
         println!("{command}: command not found");
+    
+        // let lines = io::stdin().lines();
+        // for line in lines {
+        //     let command = line.unwrap();
+        //     println!("{command}: command not found");
+        // }   
     }
 }
