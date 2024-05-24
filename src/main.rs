698096ee521d126bce569e6ff13352c1a6a75e0d
Tomasz <tspamm3r@gmail.com>
2024-05-24 22:18:58 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,22 @@ use std::process;
 use std::io::{self, Write};
 
 fn handle_exit_command(command: &str) {
+    //lets fancly do it
     if let Some(last_char) = command.chars().last() {
         match last_char.to_string().parse::<i32>() {
             Ok(status) => process::exit(status),
             Err(e) => println!("Failed to parse the status code: {}", e),
         }
+    } else {
+        println!("Failed to get last char of exit command (missing status code).");
+    }
+}
+
+//handle pattern matching
+fn handle_matching(command: &str) {
+    match command {
+        x  if x.to_string().contains("exit") => handle_exit_command(&command), //guard matching any "exit x" command where x stands for status code
+        _ => println!("{command}: command not found"), //default case where command is not implemented
     }
 }
 
@@ -20,12 +31,6 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-
-        let command = input.trim();
-
-        match command {
-            x  if x.to_string().contains("exit") => handle_exit_command(&command),
-            _ => println!("{command}: command not found"),
-        }
+        handle_matching(input.trim());
     }
 }
