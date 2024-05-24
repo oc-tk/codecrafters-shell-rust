dfe28d9104790fec6d7d1c147ad184e1088a8619
Tomasz <tspamm3r@gmail.com>
2024-05-24 23:08:41 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,8 @@ fn handle_echo_command(command: &str) {
 }
 
 fn handle_type_command(command: &str) {
-    let typeless_command = command.trim().replace("type ", "");
+    let typeless_command = command.replace("type ", "");
+    println!("{typeless_command}");
     println!("{typeless_command} is a shell builtin");
 }
 
