743145835b9ab8f8984b00de7a5333cda495be74
Tomasz <tspamm3r@gmail.com>
2024-05-24 23:22:39 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,33 @@
 use std::process;
 use std::io::{self, Write};
 
+enum Command {
+    Echo,
+    Type,
+    Exit,
+}
+
+impl Command {
+    // Convert a &str to a Command enum
+    fn from_str(command: &str) -> Option<Command> {
+        match command {
+            "echo" => Some(Command::Echo),
+            "type" => Some(Command::Type),
+            "exit" => Some(Command::Exit),
+            _ => None,
+        }
+    }
+
+    // Convert the Command enum to a &str
+    fn as_str(&self) -> &str {
+        match self {
+            Command::Echo => "echo",
+            Command::Type => "type",
+            Command::Exit => "exit",
+        }
+    }
+}
+
 fn handle_exit_command(command: &str) {
     //lets fancly do it
     if let Some(last_char) = command.chars().last() {
@@ -27,21 +54,33 @@ fn handle_type_command(command: &str) {
 
 //handle pattern matching
 fn handle_matching(input: &str) {
-    if let Some(command) = input.split_whitespace().next() {
-        //normally interpreting first part of entry
+    if let Some(command) = Command::from_str(input.split_whitespace().next().unwrap()) {
         match command {
-            x  if x.to_string().contains("exit") => handle_exit_command(&input), //guard matching any "exit x" command where x stands for status code
-            x  if x.to_string().contains("echo") => handle_echo_command(&input), //guard matching any "echo x" command where x stands string to be printed
-            x  if x.to_string().contains("type") => handle_type_command(&input), //guard matching any "type x" command where x stands for string to be printed with specific type
-            _ => println!("{command}: command not found"), //default case where command is not implemented
+            Command::Echo => handle_echo_command(&input),
+            Command::Type => handle_type_command(&input),
+            Command::Exit => handle_exit_command(&input),
         }
     } else {
-        //case when someone inserts only one command which can be exit or nothing (for now)
-        match input {
-            x  if x.to_string().contains("exit") => handle_exit_command(&input), //guard matching any "exit x" command where x stands for status code
-            _ => println!("{input}: command not found"), //default case where command is not implemented
-        }
+        println!("{input}: command not found")
     }
+
+    // if let Some(first_word) = input.split_whitespace().next() {
+        
+
+    //     //normally interpreting first part of entry
+    //     match command {
+    //         x  if x.to_string().contains("exit") => handle_exit_command(&input), //guard matching any "exit x" command where x stands for status code
+    //         x  if x.to_string().contains("echo") => handle_echo_command(&input), //guard matching any "echo x" command where x stands string to be printed
+    //         x  if x.to_string().contains("type") => handle_type_command(&input), //guard matching any "type x" command where x stands for string to be printed with specific type
+    //         _ => println!("{input}: command not found"), //default case where command is not implemented
+    //     }
+    // } else {
+    //     //case when someone inserts only one command which can be exit or nothing (for now)
+    //     match input {
+    //         x  if x.to_string().contains("exit") => handle_exit_command(&input), //guard matching any "exit x" command where x stands for status code
+    //         _ => println!("{input}: command not found"), //default case where command is not implemented
+    //     }
+    // }
     
 }
 
